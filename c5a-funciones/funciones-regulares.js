503bada5d09548d660a19c40a4deba93ac4a5656
kashipu <graficowm@gmail.com>
2022-03-10 20:30:26 -0500

    Clase de funciones

--- c5a-funciones/funciones-regulares.js
+++ c5a-funciones/funciones-regulares.js
@@ -1,1 +1,30 @@
-console.log("Funciones Regulares")
+function anterior(num) {
+    return num - 1
+}
+
+function triple(num) {
+    return num * 3
+}
+
+function anteriorDelTriple(num) {
+    let state = num
+    triple(state)
+    anterior(state)
+
+    return console.log(state)
+
+}
+
+
+// Arrow functions 
+
+// forma clásica
+function sumar(a, b) {
+    return a + b;
+}
+console.log(sumar(2, 4)); // 6
+
+// ES6 arrow function
+let sumarArrow = (a, b) => a + b;
+
+console.log(sumarArrow(2, 5)); // 6
